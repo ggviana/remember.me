4ddc3173f00155b09d32f419a70bb00587047e02
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2016-10-17 19:53:57 -0200

    Enabling build

--- src/createApplication.js
+++
@@ -1,8 +0,0 @@
-export default (document) => {
-  var application = document.createElement('div')
-  application.id = 'application'
-
-  document.body.appendChild(application)
-  
-  return application
-}
